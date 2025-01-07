85ceb9f24dcd12f393e102205dbd550122932966
AnhHoang <vutrianhhoang@gmail.com>
2025-01-07 21:10:31 +0700

    test gemini

--- tests/test_gemini/test_async.py
+++ tests/test_gemini/test_async.py
@@ -3,36 +3,36 @@ import re
 import json
 from playwright.async_api import expect
 from aionui import AiOnUi
-from aionui.models_async import ClaudeAsync
+from aionui.models_async import GeminiAsync
 from pathlib import Path
 import pytest_asyncio
 
 
 @pytest_asyncio.fixture(scope="class", loop_scope="class")
-async def claude_async(config_file):
+async def gemini_async(config_file):
     aionui = AiOnUi(config_file)
-    async with aionui.model_async("claude") as model:
+    async with aionui.model_async("gemini") as model:
         yield model
 
 
 class TestClaudeAsync:
     @pytest.mark.asyncio(loop_scope="class")
-    async def test_init_instructions(self, claude_async: ClaudeAsync):
-        await claude_async.init_instructions()
-        await expect(claude_async.page.locator(selector=".font-claude-message").last).to_have_text(re.compile(".+"))
+    async def test_init_instructions(self, gemini_async: GeminiAsync):
+        await gemini_async.init_instructions()
+        await expect(gemini_async.page.locator("model-response").last).to_have_text(re.compile(".+"))
 
     @pytest.mark.asyncio(loop_scope="class")
     @pytest.mark.dependency(depends=["test_init_instructions"])
-    async def test_chat_text(self, claude_async: ClaudeAsync):
-        result = await claude_async.chat(
+    async def test_chat_text(self, gemini_async: GeminiAsync):
+        result = await gemini_async.chat(
             "Trả lời chính xác từ sau, không thêm bất kỳ thông tin hoặc dấu câu nào khác: `Xin chào`",
         )
         assert result == "Xin chào"
 
     @pytest.mark.asyncio(loop_scope="class")
     @pytest.mark.dependency(depends=["test_chat_text"])
-    async def test_chat_code(self, claude_async: ClaudeAsync):
-        result = await claude_async.chat(
+    async def test_chat_code(self, gemini_async: GeminiAsync):
+        result = await gemini_async.chat(
             'Trả lời lại chính xác json sau, không thêm bất kỳ thông tin nào khác: `{"message": "Xin chào"}`',
             "code",
         )
@@ -41,15 +41,15 @@ class TestClaudeAsync:
 
     @pytest.mark.asyncio(loop_scope="class")
     @pytest.mark.dependency(depends=["test_chat_code"])
-    async def test_attach_file(self, claude_async: ClaudeAsync, content_file):
-        await claude_async.page.goto(claude_async.url, wait_until="networkidle")
+    async def test_attach_file(self, gemini_async: GeminiAsync, content_file):
+        await gemini_async.page.goto(gemini_async.url, wait_until="networkidle")
         path = Path(content_file)
-        await claude_async.attach_file(content_file)
-        await expect(claude_async.page.locator(f'[data-testid="{path.name}"]')).to_be_visible()
+        await gemini_async.attach_file(content_file)
+        await expect(gemini_async.page.locator(f'[data-testid="{path.name}"]')).to_be_visible()
 
     @pytest.mark.asyncio(loop_scope="class")
     @pytest.mark.dependency(depends=["test_attach_file"])
-    async def test_text_as_file(self, claude_async: ClaudeAsync):
-        await claude_async.page.goto(claude_async.url, wait_until="networkidle")
-        await claude_async.text_as_file("Xin chào")
-        await expect(claude_async.page.locator(f'[data-testid="attachment.txt"]')).to_be_visible()
+    async def test_text_as_file(self, gemini_async: GeminiAsync):
+        await gemini_async.page.goto(gemini_async.url, wait_until="networkidle")
+        await gemini_async.text_as_file("Xin chào")
+        await expect(gemini_async.page.locator(f'[data-testid="attachment.txt"]')).to_be_visible()
---
+++ tests/test_gemini/test_sync.py
@@ -0,0 +1,49 @@
+import pytest
+import re
+import json
+from playwright.sync_api import expect
+from aionui import AiOnUi
+from aionui.models import Gemini
+from pathlib import Path
+
+
+@pytest.fixture(scope="class")
+def gemini(config_file):
+    aionui = AiOnUi(config_file)
+    with aionui.model_sync("gemini") as model:
+        yield model
+
+
+class TestClaude:
+    def test_init_instructions(self, gemini: Gemini):
+        gemini.init_instructions()
+        expect(gemini.page.locator("model-response").last).to_have_text(re.compile(".+"))
+
+    @pytest.mark.dependency(depends=["test_init_instructions"])
+    def test_chat_text(self, gemini: Gemini):
+        result = gemini.chat(
+            "Trả lời chính xác từ sau, không thêm bất kỳ thông tin hoặc dấu câu nào khác: `Xin chào`",
+        )
+        assert result == "Xin chào"
+
+    @pytest.mark.dependency(depends=["test_chat_text"])
+    def test_chat_code(self, gemini: Gemini):
+        result = gemini.chat(
+            'Trả lời lại chính xác json sau, không thêm bất kỳ thông tin nào khác: `{"message": "Xin chào"}`',
+            "code",
+        )
+        dict_result = json.loads(result)
+        assert dict_result == {"message": "Xin chào"}
+
+    @pytest.mark.dependency(depends=["test_chat_code"])
+    def test_attach_file(self, gemini: Gemini, content_file):
+        gemini.page.goto(gemini.url, wait_until="networkidle")
+        path = Path(content_file)
+        gemini.attach_file(content_file)
+        expect(gemini.page.locator(f'[data-test-id="file-name"][title="{path.name}"]')).to_be_visible()
+
+    @pytest.mark.dependency(depends=["test_attach_file"])
+    def test_text_as_file(self, gemini: Gemini):
+        gemini.page.goto(gemini.url, wait_until="networkidle")
+        gemini.text_as_file("Xin chào")
+        expect(gemini.page.locator(f'[data-test-id="file-name"][title="attachment.txt"]')).to_be_visible()
